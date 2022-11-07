60e6a1f5e66347af2de560713adbafff9e7cb625
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-11-07 19:33:26 -0300

    Atualizacao de metodo registerClient

--- client.rb
+++ client.rb
@@ -4,7 +4,7 @@ module ClientRules
 end 
 class Client 
     include ClientRules
-    #private attr_reader :name, :age, :diagnosis
+    #private attr_accessor :name, :age, :diagnosis
 
     def initialize(name, age, diagnosis)
         @name = name
@@ -14,16 +14,16 @@ class Client
 
     def registerClient
         if @age <= MAX_AGE && DIAGNOSIS.include?(@diagnosis)
-            puts "🗓 Cadastro #{@name} realizado com sucesso. Boas vindas a Genial "
+            return "🗓 Cadastro de #{@name} foi realizado com sucesso. Boas vindas a Genial "
         else
-            puts "🚨 O cadastro #{@name} nao pode ser realizado porque a crianca nao tem diagnostico."
+            return "🚨 O cadastro de #{@name} nao pode ser realizado porque a crianca nao tem diagnostico."
         end 
     end 
 end
 
 
 @register1 = Client.new("Flavia Cristina", 3,'LEVEL 1')
-@register2 = Client.new("Joao Sampaio", 6, 'LEVEL 3')
+@register2 = Client.new("Joao Sampaio", 5, " ")
 
 puts @register1.registerClient
 puts @register2.registerClient
