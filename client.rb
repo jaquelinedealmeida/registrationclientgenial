3491ea132d4a7568852c06730ef65c66494198d3
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-11-08 09:26:44 -0300

    Inclusao de Expection no rescue

--- client.rb
+++ client.rb
@@ -5,7 +5,6 @@ module ClientRules
         "TEA_3" => 'Level 3',
         #"No_diagnosis" => 'No diagnosis'
     }
-
     MAX_AGE = 5
 end 
 class Client 
@@ -21,10 +20,14 @@ class Client
    
     def registerClient
         if @age <= MAX_AGE && DIAGNOSIS.fetch(@diagnosis)
-            return "🗓 Cadastro de #{@name} foi realizado com sucesso. Boas vindas a Genial "
+            "🗓 Cadastro de #{@name} foi realizado com sucesso. Boas vindas a Genial "
         end
-        rescue
-            return "🚨 O cadastro de #{@name} nao pode ser realizado porque a crianca nao tem diagnostico."
+        rescue Exception => e
+            puts "🚨 O cadastro de #{@name} nao pode ser realizado porque a crianca nao tem diagnostico."
+            puts e.message
+            #com o expection, o erro do fetch e tratado, aparecendo a msg => key not found
+            #se for inserido o retur, nao aparece a msg de tratamento de erro, assim como se nao tiver o exception 
+
     end 
 end
 
@@ -32,13 +35,14 @@ end
 @register1 = Client.new("Flavia Cristina",3,'TEA_1')
 @register2 = Client.new("Joao Sampaio", 5,'TEA_2')
 @register3 = Client.new("Samara Santos", 5,'No_diagnosis')
+@register4 = Client.new("Maria Cinara", 2,false)
 
-# e possivel chamar uma chave especifica do hash com o fetch dentro de uma condicao?
+#e possivel chamar uma chave especifica do hash com o fetch dentro de uma condicao?
 #DIAGNOSIS[:No_diagnosis]
-
 
 puts @register1.registerClient
 puts @register2.registerClient
 puts @register3.registerClient
+puts @register4.registerClient
 
-#puts @register2.registerClient("Gabriel Rodrigues", 5, false)
+
