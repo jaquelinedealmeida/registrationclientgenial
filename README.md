4b9c3346aa2861777403fa8eb1c886b8feecafb9
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-11-08 12:07:45 -0300

    Inclusao da regra dos cuidadores para cadastro das criancas

---
+++ README.md
@@ -0,0 +1,19 @@
+# DESAFIO REGISTRATION CLIENTS GENIAL
+
+## Primeira task
+
+- Criar uma solução que valide se a criança pode fazer parte do produto Genial, sendo as regras:
+1 - Apenas crianças de 0 - 5 anos podem entrar no produto
+2 - Precisa ter diagnóstico
+
+## Segunda task
+- Agora que temos as primeiras regras, o CEO, Kenny, disse que novas regras(as antigas regras devem permanecer) devem ser inseridas para permitir outras crianças entrarem na Genial, segue:
+
+1 - Os cuidadores precisam ter plano de saúde apenas da GNDI, Alice ou Particular;
+2 - Se for Particular, os cuidadores precisam ter renda maior que 10k;
+3 - Nenhum dos cuidadores pode estar desempregado.
+
+### Premissas:
+
+1 - Nenhum método pode ter mais de 6 linhas (exceção do initialize, porém não pode ter regras dentro dele);
+2 - Nenhuma classe pode ter mais de 100 linhas.
--- client.rb
+++ client.rb
@@ -1,4 +1,4 @@
-module ClientRules
+module Children_Rules
     DIAGNOSIS = {
         'TEA_1' => 'Level 1',
         "TEA_2" => 'Level 2',
@@ -6,9 +6,10 @@ module ClientRules
         #"No_diagnosis" => 'No diagnosis'
     }
     MAX_AGE = 5
-end 
-class Client 
-    include ClientRules
+end
+
+class Children 
+    include Children_Rules
     #private attr_accessor :name, :age, :diagnosis
     
     def initialize(name, age, diagnosis)
@@ -17,32 +18,76 @@ class Client
         @diagnosis = diagnosis
     end
 
-   
-    def registerClient
+    def register_children
         if @age <= MAX_AGE && DIAGNOSIS.fetch(@diagnosis)
             "🗓 Cadastro de #{@name} foi realizado com sucesso. Boas vindas a Genial "
         end
         rescue Exception => e
             puts "🚨 O cadastro de #{@name} nao pode ser realizado porque a crianca nao tem diagnostico."
             puts e.message
-            #com o expection, o erro do fetch e tratado, aparecendo a msg => key not found
-            #se for inserido o retur, nao aparece a msg de tratamento de erro, assim como se nao tiver o exception 
-
     end 
 end
 
+module Rules_Caregivers
 
-@register1 = Client.new("Flavia Cristina",3,'TEA_1')
-@register2 = Client.new("Joao Sampaio", 5,'TEA_2')
-@register3 = Client.new("Samara Santos", 5,'No_diagnosis')
-@register4 = Client.new("Maria Cinara", 2,false)
+HEALTH_PLAN = ['GNDI', 'Alice', 'Particular']
+INCOME_CAREGIVERS = 10.000
+EMPLOYED_CAREGIVERS = true
+end 
 
-#e possivel chamar uma chave especifica do hash com o fetch dentro de uma condicao?
-#DIAGNOSIS[:No_diagnosis]
+class Caregivers
+include Rules_Caregivers
+    
+    def initialize(children,health_plan, income, employed_caregivers)
+        @children = Children
+        @health_plan = health_plan
+        @income = income 
+        @employed_caregivers = employed_caregivers
+    end 
 
-puts @register1.registerClient
-puts @register2.registerClient
-puts @register3.registerClient
-puts @register4.registerClient
+    def register_Caregivers
+        if HEALTH_PLAN.include?(@health_plan)
+            return "Seu cadastro foi aprovado."
+        else 
+            return "Seu plano nao e um dos nossos parceiros"
+        end 
+    end 
+
+    def particular_plan
+        if @income >= INCOME_CAREGIVERS && @employed_caregivers === EMPLOYED_CAREGIVERS
+            return "Cadastro realizado com sucesso"
+        else 
+            return "Seu perfil nao esta dentro dos criterios da Genial."
+        end
+    end 
+end 
+
+@register1 = Children.new("Flavia Cristina",3,'TEA_1')
+@register2 = Children.new("Samara Santos", 5,'No_diagnosis')
+
+puts @register1.register_children
+puts @register2.register_children
+
+@register1 = Caregivers.new(@children, "Alice", 10.000, true)
+@register2 = Caregivers.new(@children, "Bradesco", 10.000, true)
+
+puts @register1.register_Caregivers
+puts @register2.register_Caregivers
+
+@register3 = Caregivers.new(@children, "Particular", 10.000, true)
+@register4 = Caregivers.new(@children, "Particular", 10.000, false)
+
+puts @register3.particular_plan
+puts @register4.particular_plan
+
+
+
+
+
+
+
+
+
+
 
 
